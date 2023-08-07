a8bb1e4d26044d6f8279f123d679d41df3f6eeaf
Julio jose Middagh <juliomiddagh@gmail.com>
2023-08-06 21:28:44 -0300

    Add file html and js

---
+++ juego.js
@@ -0,0 +1,46 @@
+// Función para obtener la elección aleatoria de la computadora
+function getComputerChoice() {
+    const choices = ['piedra', 'papel', 'tijera'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+  }
+  
+  // Función para convertir la elección del usuario en palabra legible
+  function convertToWord(choice) {
+    if (choice === 'piedra') return 'Piedra';
+    if (choice === 'papel') return 'Papel';
+    return 'Tijera';
+  }
+  
+  // Función para determinar el resultado del juego
+  function checkResult(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return 'Es un empate!';
+    } else if (
+      (userChoice === 'piedra' && computerChoice === 'tijera') ||
+      (userChoice === 'papel' && computerChoice === 'piedra') ||
+      (userChoice === 'tijera' && computerChoice === 'papel')
+    ) {
+      return '¡Ganaste!';
+    } else {
+      return 'Perdiste. Intenta de nuevo.';
+    }
+  }
+  
+  // Función principal para jugar el juego
+  function playGame(userChoice) {
+    const computerChoice = getComputerChoice();
+    const result = checkResult(userChoice, computerChoice);
+  
+    document.getElementById('result').innerHTML = `
+      <p>Tu elección: ${convertToWord(userChoice)}</p>
+      <p>Elección de la computadora: ${convertToWord(computerChoice)}</p>
+      <p>${result}</p>
+    `;
+  }
+  
+  // Evento de clic para las opciones del juego
+  document.getElementById('piedra').addEventListener('click', () => playGame('piedra'));
+  document.getElementById('papel').addEventListener('click', () => playGame('papel'));
+  document.getElementById('tijera').addEventListener('click', () => playGame('tijera'));
+  
